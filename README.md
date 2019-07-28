d9bb2781fba1102de21073d78281284f9f542e55
Brandon Bachrach <brandon.bachrach@gmail.com>
2019-07-27 22:50:48 -0400

    Dependency Security Updates and Styling fixes

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 A 10-minute React app to count the amount of time this guy in the other room says "Right".
 
+Things I learned:
+How to add "memory" features to an app
+How to create an app quickly and efficiently
+
 To do:
 Add option to choose the keyword to count.
--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
-      remember: 0
-    }
+      memory: 0
+    };
   }
 
   incrementCount(e) {
     this.setState({
-      count:this.state.count + 1
+      count: this.state.count + 1
     });
   }
 
   incrementCountThree(e) {
     this.setState({
-      count:this.state.count + 3
+      count: this.state.count + 3
     });
   }
+
   decrementCount(e) {
-    if(this.state.count > 0) {
+    if (this.state.count > 0) {
       this.setState({
-        count:this.state.count - 1
+        count: this.state.count - 1
       });
     }
   }
 
   resetCount(e) {
     this.setState((prevState, props) => ({
-      count:0,
-      remember: this.state.count
+      count: 0,
+      memory: this.state.count ? this.state.count : this.state.memory
     }));
   }
 
   restoreCount(e) {
-    this.setState( (prevState,props) => ({
-      count: this.state.remember
+    this.setState((prevState, props) => ({
+      count: this.state.memory
     }));
   }
 
   restoreCountAndAdd(e) {
-    this.setState( (prevState,props) => ({
-      count: this.state.remember + this.state.count
+    this.setState((prevState, props) => ({
+      count: this.state.memory + this.state.count
     }));
   }
 
@@ -54,11 +55,15 @@ class App extends Component {
         <h2>That guy just said "Right"</h2>
         <h1>{this.state.count} TIMES</h1>
         <button onClick={this.incrementCount.bind(this)}>"Right"</button>
-        <button onClick={this.incrementCountThree.bind(this)}>"Right Right Right"</button>
+        <button onClick={this.incrementCountThree.bind(this)}>
+          "Right Right Right"
+        </button>
         <button onClick={this.decrementCount.bind(this)}>Woops</button>
         <button onClick={this.resetCount.bind(this)}>Reset</button>
         <button onClick={this.restoreCount.bind(this)}>Restore</button>
-        <button onClick={this.restoreCountAndAdd.bind(this)}>Restore and Add</button>
+        <button onClick={this.restoreCountAndAdd.bind(this)}>
+          Restore and Add
+        </button>
       </div>
     );
   }
