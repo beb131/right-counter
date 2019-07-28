29a1a65256fca1f8d7a105078ee6ecb92ce86f11
Brandon Bachrach <brandon.bachrach@gmail.com>
2019-07-27 22:06:28 -0400

    Version 1

---
+++ README.md
@@ -0,0 +1,4 @@
+A 10-minute React app to count the amount of time this guy in the other room says "Right".
+
+To do:
+Add option to choose the keyword to count.
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      remember: 0
+    }
+  }
+
+  incrementCount(e) {
+    this.setState({
+      count:this.state.count + 1
+    });
+  }
+
+  incrementCountThree(e) {
+    this.setState({
+      count:this.state.count + 3
+    });
+  }
+  decrementCount(e) {
+    if(this.state.count > 0) {
+      this.setState({
+        count:this.state.count - 1
+      });
+    }
+  }
+
+  resetCount(e) {
+    this.setState((prevState, props) => ({
+      count:0,
+      remember: this.state.count
+    }));
+  }
+
+  restoreCount(e) {
+    this.setState( (prevState,props) => ({
+      count: this.state.remember
+    }));
+  }
+
+  restoreCountAndAdd(e) {
+    this.setState( (prevState,props) => ({
+      count: this.state.remember + this.state.count
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>That guy just said "Right"</h2>
+        <h1>{this.state.count} TIMES</h1>
+        <button onClick={this.incrementCount.bind(this)}>"Right"</button>
+        <button onClick={this.incrementCountThree.bind(this)}>"Right Right Right"</button>
+        <button onClick={this.decrementCount.bind(this)}>Woops</button>
+        <button onClick={this.resetCount.bind(this)}>Reset</button>
+        <button onClick={this.restoreCount.bind(this)}>Restore</button>
+        <button onClick={this.restoreCountAndAdd.bind(this)}>Restore and Add</button>
+      </div>
+    );
+  }
+}
+
+export default App;
